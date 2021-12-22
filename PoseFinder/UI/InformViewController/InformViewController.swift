6c94d879c1bab5cbcdd470c77fa178abb0d598a8
gomigom1068 <77622994+gomigom1068@users.noreply.github.com>
2021-12-22 14:28:18 +0900

    omg

---
+++ PoseFinder/UI/InformViewController/InformViewController.swift
@@ -0,0 +1,145 @@
+//
+//  InformViewController.swift
+//  PoseDetectionProto
+//
+//  Created by 정은서 on 2021/11/25.
+//
+
+import UIKit
+
+class InformViewController : UIViewController {
+    var exerKind: String!
+    
+    var informview: UIImageView!
+    var pageControl: UIPageControl!
+    var images = ["s1", "s2", "s3"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        //checkingView()
+        informView()
+        addNextButton()
+        addPrevButton()
+        addOKButton()
+        
+        if exerKind == "SQUAT"{
+        }
+    }
+    func addNextButton(){
+        let nextButton = UIButton(frame: CGRect(x: view.frame.width * 0.7,
+                                              y: view.frame.height * 0.8,
+                                              width: view.frame.width * 0.2,
+                                              height: view.frame.height * 0.05))
+        nextButton.setTitle("다음", for: UIControl.State.normal)
+        nextButton.setTitleColor(.white, for: UIControl.State.normal)
+        nextButton.backgroundColor = .lightGray
+        nextButton.alpha = 0.7
+        nextButton.addTarget(self, action: #selector(pageChangeup), for :UIControl.Event.touchUpInside)
+        view.addSubview(nextButton)
+    }
+    func addPrevButton(){
+        let prevButton = UIButton(frame: CGRect(x: view.frame.width * 0.1,
+                                              y: view.frame.height * 0.8,
+                                              width: view.frame.width * 0.2,
+                                              height: view.frame.height * 0.05))
+        prevButton.setTitle("이전", for: UIControl.State.normal)
+        prevButton.setTitleColor(.white, for: UIControl.State.normal)
+        prevButton.backgroundColor = .lightGray
+        prevButton.alpha = 0.7
+        prevButton.addTarget(self, action: #selector(pageChangedown), for : UIControl.Event.touchUpInside)
+        view.addSubview(prevButton)
+    }
+    
+    func informView(){
+        informview = UIImageView(frame: CGRect(x: view.frame.minX,
+                                               y: view.frame.height * 0,
+                                               width: view.frame.width,
+                                               height: view.frame.height * 0.8))
+        informview.contentMode = .scaleAspectFit
+        
+        pageControl = UIPageControl(frame: CGRect(x: 0,
+                                                y: self.view.frame.maxY - 215,
+                                                width: self.view.frame.maxX,
+                                                height:50))
+
+        pageControl.numberOfPages = 3
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        informview.image = UIImage(named: images[pageControl.currentPage])
+        
+        pageControl.addTarget(self, action: #selector(pageChangeup), for: .touchDragEnter)
+        pageControl.addTarget(self, action: #selector(pageChangedown), for: .touchDragExit)
+
+        
+        view.addSubview(self.informview)
+        view.addSubview(self.pageControl)
+    }
+    
+    @objc
+    func pageChangeup(){
+        pageControl.currentPage += 1
+        informview.image = UIImage(named: images[pageControl.currentPage])
+    }
+    @objc
+    func pageChangedown(){
+        pageControl.currentPage -= 1
+        informview.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+
+    func checkingView(){
+        let label = UILabel(frame: CGRect(x: view.frame.width*0.2, y: view.frame.height*0.05, width: view.frame.width * 0.6, height: view.frame.height * 0.1))
+        label.text = exerKind
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = UIFont(name: "Apple Color Emoji", size: 30)
+        label.backgroundColor = .blue
+        label.alpha = 0.7
+        label.layer.cornerRadius = 20
+        
+        
+        let inform = UIImage(named: "information")
+        let informview = UIImageView(frame: CGRect(x: view.frame.minX,
+                                                   y: view.frame.height * 0.1,
+                                                   width: view.frame.width,
+                                                   height: view.frame.height * 0.8))
+        informview.contentMode = .scaleAspectFit
+        informview.image = inform
+        
+        view.addSubview(label)
+        view.addSubview(informview)
+        print(exerKind)
+    }
+    
+    func addOKButton(){
+        let okButton = UIButton(frame: CGRect(x: view.frame.width * 0.4,
+                                              y: view.frame.height * 0.8,
+                                              width: view.frame.width * 0.2,
+                                              height: view.frame.height * 0.05))
+        okButton.setTitle("OK", for: UIControl.State.normal)
+        okButton.setTitleColor(.white, for: UIControl.State.normal)
+        okButton.backgroundColor = .gray
+        okButton.alpha = 0.7
+        okButton.addTarget(self, action: #selector(toPoseDetectionView), for: UIControl.Event.touchUpInside)
+        view.addSubview(okButton)
+    }
+    
+    @objc func toPoseDetectionView(_ sender: UIButton){
+        performSegue(withIdentifier: "DetectViewSegue", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "DetectViewSegue" {
+            if let vc = segue.destination as? ViewController {
+                vc.exerKind = exerKind
+            }
+        }
+    }
+    /*
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    */
+}
---
+++ PoseFinder/UI/PoseImageView.swift
@@ -0,0 +1,225 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Implementation details of a view that visualizes the detected poses.
+*/
+
+import UIKit
+
+@IBDesignable
+class PoseImageView: UIImageView {
+    var check = 0
+    var count = 0
+    var feedlabel = "normal"
+//    var label: UILabel!
+    /// A data structure used to describe a visual connection between two joints.
+    struct JointSegment {
+        let jointA: Joint.Name
+        let jointB: Joint.Name
+    }
+
+    /// An array of joint-pairs that define the lines of a pose's wireframe drawing.
+    static let jointSegments = [
+        // The connected joints that are on the left side of the body.
+        JointSegment(jointA: .leftHip, jointB: .leftShoulder),
+        JointSegment(jointA: .leftShoulder, jointB: .leftElbow),
+        JointSegment(jointA: .leftElbow, jointB: .leftWrist),
+        JointSegment(jointA: .leftHip, jointB: .leftKnee),
+        JointSegment(jointA: .leftKnee, jointB: .leftAnkle),
+        // The connected joints that are on the right side of the body.
+        JointSegment(jointA: .rightHip, jointB: .rightShoulder),
+        JointSegment(jointA: .rightShoulder, jointB: .rightElbow),
+        JointSegment(jointA: .rightElbow, jointB: .rightWrist),
+        JointSegment(jointA: .rightHip, jointB: .rightKnee),
+        JointSegment(jointA: .rightKnee, jointB: .rightAnkle),
+        // The connected joints that cross over the body.
+        JointSegment(jointA: .leftShoulder, jointB: .rightShoulder),
+        JointSegment(jointA: .leftHip, jointB: .rightHip)
+    ]
+
+    /// The width of the line connecting two joints.
+    @IBInspectable var segmentLineWidth: CGFloat = 2
+    /// The color of the line connecting two joints.
+    @IBInspectable var segmentColor: UIColor = UIColor.systemTeal
+    /// The radius of the circles drawn for each joint.
+    @IBInspectable var jointRadius: CGFloat = 4
+    /// The color of the circles drawn for each joint.
+    @IBInspectable var jointColor: UIColor = UIColor.systemPink
+
+    // MARK: - Rendering methods
+
+    /// Returns an image showing the detected poses.
+    ///
+    /// - parameters:
+    ///     - poses: An array of detected poses.
+    ///     - frame: The image used to detect the poses and used as the background for the returned image.
+    func show(poses: [Pose], on frame: CGImage, exer: String, label: UILabel){
+        //print(poses)
+        let dstImageSize = CGSize(width: frame.width, height: frame.height)
+        let dstImageFormat = UIGraphicsImageRendererFormat()
+
+        dstImageFormat.scale = 1
+        let renderer = UIGraphicsImageRenderer(size: dstImageSize,
+                                               format: dstImageFormat)
+
+        let dstImage = renderer.image { rendererContext in
+            // Draw the current frame as the background for the new image.
+            draw(image: frame, in: rendererContext.cgContext)
+
+            for pose in poses {
+                // Draw the segment lines.
+                for segment in PoseImageView.jointSegments {
+                    let jointA = pose[segment.jointA]
+                    let jointB = pose[segment.jointB]
+
+                    guard jointA.isValid, jointB.isValid else {
+                        continue
+                    }
+
+                    drawLine(from: jointA,
+                             to: jointB,
+                             in: rendererContext.cgContext)
+                
+                }
+                
+
+                // Draw the joints as circles above the segment lines.
+                for joint in pose.joints.values.filter({ $0.isValid }) {
+                    draw(circle: joint, in: rendererContext.cgContext)
+                    print(joint.name,joint.position.y, joint.position.x)
+                   
+                }
+                check += 1
+                print(check)
+                print(frame.height)
+                print(frame.width)
+                
+                countExercise(exer: exer, pose: pose)
+                label.text = String(count)
+                
+            }
+        }
+        
+        
+
+        image = dstImage
+    }
+    
+    func countExercise(exer: String, pose: Pose) {
+        if(exer=="SQUAT"){
+            if readyform(exer: exer, pose: pose) {
+                if feedlabel == "motioning"{
+                    count = count + 1
+                }
+                
+                feedlabel = "ready"
+                
+            }else if exerform(exer: exer, pose: pose){
+                feedlabel = "motioning"
+            }
+        }
+        globalcount = count
+        globalFeed = feedlabel
+    }
+    
+    func readyform(exer: String, pose: Pose) -> Bool {
+        // right-Shoulder-Hip-Knee angle
+        if let fl = pose.joints[.rightShoulder], let sl = pose.joints[.rightHip], let tl = pose.joints[.rightKnee]{
+            let RSHK = angle(firstLandmark: fl, midLandmark: sl, lastLandmark: tl )
+            if(RSHK>=160){
+                print("RSHK ready ok")
+                return true
+
+            }else{
+                print("no")
+                return false
+            }
+        }
+        
+        return false
+        
+    }
+    
+    func exerform(exer: String, pose: Pose) -> Bool {
+        if let fl = pose.joints[.rightShoulder], let sl = pose.joints[.rightHip], let tl = pose.joints[.rightKnee]{
+            let RSHK = angle(firstLandmark: fl, midLandmark: sl, lastLandmark: tl )
+            if(45<=RSHK&&RSHK<=90){
+                print("RSHK exer ok")
+                return true
+
+            }else{
+                print("no")
+                return false
+            }
+        }
+        
+        return false
+        
+    }
+    
+    func angle(
+          firstLandmark: Joint,
+          midLandmark: Joint,
+          lastLandmark: Joint
+      ) -> CGFloat {
+          let radians: CGFloat =
+              atan2(lastLandmark.position.y - midLandmark.position.y,
+                        lastLandmark.position.x - midLandmark.position.x) -
+                atan2(firstLandmark.position.y - midLandmark.position.y,
+                        firstLandmark.position.x - midLandmark.position.x)
+          var degrees = radians * 180.0 / .pi
+          degrees = abs(degrees) // Angle should never be negative
+          if degrees > 180.0 {
+              degrees = 360.0 - degrees // Always get the acute representation of the angle
+          }
+          return degrees
+      }
+
+    /// Vertically flips and draws the given image.
+    ///
+    /// - parameters:
+    ///     - image: The image to draw onto the context (vertically flipped).
+    ///     - cgContext: The rendering context.
+    func draw(image: CGImage, in cgContext: CGContext) {
+        cgContext.saveGState()
+        // The given image is assumed to be upside down; therefore, the context
+        // is flipped before rendering the image.
+        cgContext.scaleBy(x: 1.0, y: -1.0)
+        // Render the image, adjusting for the scale transformation performed above.
+        let drawingRect = CGRect(x: 0, y: -image.height, width: image.width, height: image.height)
+        cgContext.draw(image, in: drawingRect)
+        cgContext.restoreGState()
+    }
+
+    /// Draws a line between two joints.
+    ///
+    /// - parameters:
+    ///     - parentJoint: A valid joint whose position is used as the start position of the line.
+    ///     - childJoint: A valid joint whose position is used as the end of the line.
+    ///     - cgContext: The rendering context.
+    func drawLine(from parentJoint: Joint,
+                  to childJoint: Joint,
+                  in cgContext: CGContext) {
+        cgContext.setStrokeColor(segmentColor.cgColor)
+        cgContext.setLineWidth(segmentLineWidth)
+
+        cgContext.move(to: parentJoint.position)
+        cgContext.addLine(to: childJoint.position)
+        cgContext.strokePath()
+    }
+
+    /// Draw a circle in the location of the given joint.
+    ///
+    /// - parameters:
+    ///     - circle: A valid joint whose position is used as the circle's center.
+    ///     - cgContext: The rendering context.
+    private func draw(circle joint: Joint, in cgContext: CGContext) {
+        cgContext.setFillColor(jointColor.cgColor)
+
+        let rectangle = CGRect(x: joint.position.x - jointRadius, y: joint.position.y - jointRadius,
+                               width: jointRadius * 2, height: jointRadius * 2)
+        cgContext.addEllipse(in: rectangle)
+        cgContext.drawPath(using: .fill)
+    }
+}
